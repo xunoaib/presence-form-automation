60611829403d7c3d4db9b9790486f10c4ec2a197
xunoaib <xunoaib@gmail.com>
2026-02-21 17:38:56 -0500

    Cache dates which have been submitted

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import json
 import os
 import sys
 import traceback
 from datetime import date, datetime, time, timedelta
+from pathlib import Path
+from typing import Iterable
 
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -11,6 +14,11 @@ from core import fill_event_registration_form, show_submission_menu
 from eventform import EventForm
 
 load_dotenv()
+
+
+DATE_FILE = Path('submitted_dates.json')
+START_TIME = time(hour=15, minute=45)
+END_TIME = time(hour=18, minute=45)
 
 
 def next_saturday(from_date=None):
@@ -41,18 +49,44 @@ def get_options(profile_location=None, binary_location=None):
     return options
 
 
+def get_next_sat_times(submitted_dates: Iterable[str] | None = None):
+    submitted_dates = submitted_dates or []
+
+    sat = next_saturday(date.today() + timedelta(days=1))
+    while True:
+        s = sat.strftime('%Y-%m-%d')
+        if s not in submitted_dates:
+            break
+        sat += timedelta(days=7)
+
+    start_time = datetime.combine(sat, START_TIME)
+    end_time = datetime.combine(start_time, END_TIME)
+
+    return start_time, end_time
+
+
 def main():
     FORM_URL = os.environ['FORM_URL']
 
-    start_time = next_saturday(date.today() + timedelta(days=14))
-    start_time = datetime.combine(start_time, time(hour=15, minute=45))
-    end_time = datetime.combine(start_time, time(hour=18, minute=45))
+    submitted_dates = json.load(
+        DATE_FILE.open()) if DATE_FILE.exists() else []
+    assert isinstance(submitted_dates, list)
+
+    start_time, end_time = get_next_sat_times(submitted_dates)
+
+    print('Start:', start_time)
+    print('End:  ', end_time)
+
+    if (input('\nGood? [Y/n] ').lower() or 'y') != 'y':
+        print('exiting')
+        return
 
     data = EventForm.from_yaml(
         'config.yml',
         overrides={
             'start_time': format_datetime(start_time),
-            'end_time': format_datetime(end_time)
+            'end_time': format_datetime(end_time),
+            'about_html': Path('about.html').read_text(),
         }
     )
 
@@ -73,18 +107,27 @@ def main():
     ).lower() != 'y':
         return
 
+    choice = None
     try:
         fill_event_registration_form(driver, data)
-        show_submission_menu(driver)
+        choice = show_submission_menu(driver)
     except Exception:
         traceback.print_exc()
     except KeyboardInterrupt:
         print('interrupted')
 
-    print()
-    print('==== Entering Debugger ====')
-    print()
-    __import__('pdb').set_trace()
+    print('philled')
+
+    if choice == '1' and (input('\nUpdate dates? [Y/n] ').lower() or 'y') == 'y':
+        print('saving')
+        submitted_dates.append(start_time.strftime('%Y-%m-%d'))
+        with DATE_FILE.open('w') as f:
+            json.dump(submitted_dates, f)
+
+    # print()
+    # print('==== Entering Debugger ====')
+    # print()
+    # __import__('pdb').set_trace()
 
 
 if __name__ == '__main__':
